b56e62a03dda3426fe8a2d531430893d2415ebfe
darrylroxx <darrylroxx@gmail.com>
2026-04-05 11:29:56 -0400

    Delete tmp-validate-2.js

--- tmp-validate-2.js
+++
@@ -1,9 +0,0 @@
-
-if('serviceWorker' in navigator){
-  window.addEventListener('load',function(){
-    navigator.serviceWorker.register('./sw.js').catch(function(err){
-      console.warn('Service worker registration failed.', err);
-    });
-  });
-}
-
